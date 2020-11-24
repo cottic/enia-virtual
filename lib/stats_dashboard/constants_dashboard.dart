d405a2072dcf22b5a2f5809038e5c23ceb25741e
FerBueroTrebino <ferbuerotrebino@gmail.com>
2020-11-24 14:07:51 +0100

    Change dashboard font to Encode Sans
    Change dashboard font to Encode Sans

--- lib/stats_dashboard/constants_dashboard.dart
+++ lib/stats_dashboard/constants_dashboard.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-TextStyle mainTitleCharts =
-    TextStyle(fontSize: 18.0, fontWeight: FontWeight.w600);
+TextStyle mainTitleCharts = GoogleFonts.encodeSans(
+  textStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 28.0),
+);
 
-TextStyle descriptionCharts = TextStyle(fontSize: 10.0, color: Colors.grey);
+TextStyle headerMain = GoogleFonts.encodeSans(
+  textStyle: TextStyle(fontWeight: FontWeight.w700, fontSize: 22.0),
+);
+
+TextStyle descriptionCharts = GoogleFonts.encodeSans(
+  textStyle: TextStyle(fontSize: 10.0, color: Colors.grey),
+);
 
 final double indicatorsDotSize = 12.0;
 
-TextStyle indicatorsText = TextStyle(fontSize: 12.0, color: Colors.grey);
+TextStyle indicatorsText = GoogleFonts.encodeSans(
+  textStyle: TextStyle(fontSize: 10.0, color: Colors.grey),
+);
 
-TextStyle leftTitlesChart = TextStyle(fontSize: 10.0, color: Colors.grey);
+TextStyle leftTitlesChart = GoogleFonts.encodeSans(
+  textStyle: TextStyle(fontSize: 10.0, color: Colors.grey),
+);
 
-TextStyle bottomTitlesChart = TextStyle(fontSize: 10.0, color: Colors.grey);
+TextStyle bottomTitlesChart = GoogleFonts.encodeSans(
+  textStyle: TextStyle(fontSize: 10.0, color: Colors.grey),
+);
