3c2eb6a3da3147a4e307912e0bb8ab8f8d5e557c
jmhlawatsch <juanma@codigoi.com.ar>
2020-12-14 09:55:26 -0300

    Dashboard 1 2 3 4 5

--- lib/stats_dashboard/dashboard_04.dart
+++ lib/stats_dashboard/dashboard_04.dart
@@ -75,15 +75,15 @@ class _StatsEniaMenu04State extends State<StatsEniaMenu04> {
 
     var dashboard = await Dashboard.fromJson(appSettingsMap);
 
-    board = dashboard.boards[0];
-    cardChart0 = dashboard.boards[0].charts[0];
-    cardChart1 = dashboard.boards[0].charts[1];
-    cardChart2 = dashboard.boards[0].charts[2];
-    cardChart3 = dashboard.boards[0].charts[3];
-    barChart = dashboard.boards[0].charts[4];
-    pieSingleChart = dashboard.boards[0].charts[5];
-    pieChart = dashboard.boards[0].charts[6];
-    lineChart = dashboard.boards[0].charts[7];
+    board = dashboard.boards[3];
+    cardChart0 = dashboard.boards[3].charts[0];
+    cardChart1 = dashboard.boards[3].charts[1];
+    cardChart2 = dashboard.boards[3].charts[2];
+    cardChart3 = dashboard.boards[3].charts[3];
+    barChart = dashboard.boards[3].charts[4];
+    pieSingleChart = dashboard.boards[3].charts[5];
+    pieChart = dashboard.boards[3].charts[6];
+    lineChart = dashboard.boards[3].charts[7];
 
     return board;
   }
