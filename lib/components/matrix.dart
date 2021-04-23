8dcf7bcf2e88884d8d304706deff15c0f06b1672
jmhlawatsch <juanma@codigoi.com.ar>
2021-04-23 07:08:40 -0300

    Add version 1.9.2

--- lib/components/matrix.dart
+++ lib/components/matrix.dart
@@ -47,7 +47,7 @@ class Matrix extends StatefulWidget {
   ];
 
   //ENIA VERSION
-  static const String versionENIA = 'Versión 1.9.1';
+  static const String versionENIA = 'Versión 1.9.2';
 
   final Widget child;
 
